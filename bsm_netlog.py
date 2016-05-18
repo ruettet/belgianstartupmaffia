2db6a92c6d7e647285a4a676d878de6072967ac4
Tom Ruette <tom.ruette@sirris.be>
2016-05-18 10:45:08 +0200

    created a netlog maffia

---
+++ bsm_netlog.py
@@ -0,0 +1,60 @@
+from py2neo import Graph, Path, Node, Relationship
+from codecs import open
+from json import dumps
+from openpyxl import load_workbook
+import sys
+
+bsm = load_workbook("/media/sf_datasets/accelerate/Belgische Startup Maffia netlog.xlsx")["Sheet1"]
+
+# post to neo4j
+graph = Graph("http://belgian_startup_maffia:FFaPiypFOOS4tRB7pBHl@belgianstartupmaffia.sb02.stations.graphenedb.com:24789/db/data/")
+
+if len(sys.argv) > 1:
+  if sys.argv[1] == "update":
+
+    graph.cypher.execute("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE r,n")
+
+    graph.schema.drop_uniqueness_constraint("Company", "name")
+    graph.schema.drop_uniqueness_constraint("Fund", "name")
+    graph.schema.drop_uniqueness_constraint("Institute", "name")
+    graph.schema.drop_uniqueness_constraint("Person", "name")
+
+    graph.schema.create_uniqueness_constraint("Company", "name")
+    graph.schema.create_uniqueness_constraint("Fund", "name")
+    graph.schema.create_uniqueness_constraint("Institute", "name")
+    graph.schema.create_uniqueness_constraint("Person", "name")
+
+    for row in bsm.rows[1:]:
+      from_type, from_name, edge_type, edge_name, to_type, to_name, netlog = [cell.value for cell in row]
+      if netlog is None:
+        from_type = "grey"
+        to_type = "grey"
+      print(from_type, from_name, edge_type, to_type, to_name)
+      from_node = graph.merge_one(from_type.strip(), "name", from_name.strip())
+      to_node = graph.merge_one(to_type.strip(), "name", to_name.strip())
+      from_to = Relationship(from_node, edge_type, to_node)
+      graph.create_unique(from_to)
+
+    # get nodes with degree
+    nodes = []
+    for label in graph.node_labels:
+      for p in graph.find(label):
+        node = {"id": p.ref.split("/")[-1],
+                "label": p["name"], 
+                "title": p["name"],
+                "value": p.degree,
+                "group": label}
+        nodes.append(node)
+    with open("report/nodesnetlog.js", "w") as f:
+      f.write("var nodesraw = " + dumps(nodes, indent=2) + ";")
+
+    # get edges
+    edges = []
+    for r in graph.match():
+      edge = {"to": r.end_node.ref.split("/")[-1],
+              "from": r.start_node.ref.split("/")[-1]
+             }
+      edges.append(edge)
+    with open("report/edgesnetlog.js", "w") as f:
+      f.write("var edgesraw = " + dumps(edges, indent=2) + ";")
+
